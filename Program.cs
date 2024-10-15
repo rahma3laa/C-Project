4e7bf1a9202676dcf4b03368bfbf0e1e9348444b
rahma3laa <rahmaaaliii55@gmail.com>
2024-10-15 22:59:35 +0300

    second repo

---
+++ Program.cs
@@ -0,0 +1,124 @@
+﻿  rock_paper_scissors_game
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Random random = new Random();
+                bool playAgain = true;
+                String player;
+                String computer;
+                String answer;
+
+                while (playAgain)
+                {
+                    player = "";
+                    computer = "";
+                    answer = "";
+
+                    while (player != "ROCK" && player != "PAPER" && player != "SCISSORS")
+                    {
+                        Console.Write("Enter ROCK, PAPER, or SCISSORS: ");
+                        player = Console.ReadLine();
+                        player = player.ToUpper();
+                    }
+
+                    int randomNumber=random.Next(1,4);
+                    switch (randomNumber)
+                    {
+                        case 1:
+                            computer = "ROCK";
+                            break;
+                        case 2:
+                            computer = "PAPER";
+                            break;
+                        case 3:
+                            computer = "SCISSORS";
+                            break;
+                    }
+
+                    Console.WriteLine("Player: " + player);
+                    Console.WriteLine("Computer: " + computer);
+
+                    switch (player)
+                    {
+                        case "ROCK":
+                            if (computer == "ROCK")
+                            {
+                                Console.WriteLine("It's a draw!");
+                            }
+                            else if (computer == "PAPER")
+                            {
+                                Console.WriteLine("You lose!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("You win!");
+                            }
+                            break;
+                        case "PAPER":
+                            if (computer == "ROCK")
+                            {
+                                Console.WriteLine("You win!");
+                            }
+                            else if (computer == "PAPER")
+                            {
+                                Console.WriteLine("It's a draw!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("You lose!");
+                            }
+                            break;
+                        case "SCISSORS":
+                            if (computer == "ROCK")
+                            {
+                                Console.WriteLine("You lose!");
+                            }
+                            else if (computer == "PAPER")
+                            {
+                                Console.WriteLine("You win!");
+                            }
+                            else
+                            {
+                                Console.WriteLine("It's a draw!");
+                            }
+                            break;
+                    }
+                    Console.WriteLine("                                      ");
+                    Console.Write("Would you like to play again (Y/N): ");
+                    answer = Console.ReadLine();
+                    answer = answer.ToUpper();
+
+                    if (answer == "Y")
+                    {
+                        playAgain = true;
+                    }
+                    else
+                    {
+                        playAgain = false;
+                    }
+                    Console.WriteLine("                                      ");
+                   
+                }
+
+                Console.WriteLine("Thanks for playing!");
+                Console.BackgroundColor = ConsoleColor.Black;
+                Console.ForegroundColor = ConsoleColor.White;
+
+
+
+            }
+    
+
+}
+
+
+
+
+    }
+}
